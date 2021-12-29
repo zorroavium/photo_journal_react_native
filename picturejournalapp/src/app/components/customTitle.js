b3e1341f78a14f07fbdb03a88fab527371a6b90e
Rahul Prasad <rahulp@polyglotconsultants.com>
2021-12-29 23:16:09 +0530

    Add customTitle component for header and splash screen

---
+++ picturejournalapp/src/app/components/customTitle.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class CustomTitle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          backgroundColor: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
+        }}>
+        <Text style={styles.label}>
+          {this.props.text1 && (
+            <Text style={styles.titleText1}>{this.props.text1}</Text>
+          )}
+          {this.props.text2 && (
+            <Text style={styles.titleText2}>{this.props.text2}</Text>
+          )}
+          {this.props.text3 && (
+            <Text style={styles.titleText3}>{this.props.text3}</Text>
+          )}
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default CustomTitle;
+
+const styles = StyleSheet.create({
+  titleText1: {
+    fontSize: 30,
+    color: '#6c6c6c',
+    fontFamily: 'Inter-Bold',
+  },
+  titleText2: {
+    fontSize: 30,
+    color: '#6c6c6c',
+    fontFamily: 'Inter-Regular',
+  },
+  titleText3: {
+    fontSize: 30,
+    color: '#00e3ba',
+    fontFamily: 'Inter-SemiBold',
+  },
+});
