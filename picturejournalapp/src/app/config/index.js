5210bd35c586d5d92d01650656ed1ee66670cd42
Rahul Prasad <rahulp@polyglotconsultants.com>
2021-12-31 21:29:50 +0530

    Add API endpoint for weather data

--- picturejournalapp/src/app/config/index.js
+++ picturejournalapp/src/app/config/index.js
@@ -1,3 +1,6 @@
+const WEATHER_API_KEY = '82f86f8855eeda9332684d7cc04d50d2';
 export const BASE_URL = 'https://example-data.draftbit.com/books?_limit=10';
 export const BASE_URL_LOCATION = (lat, lon) =>
   `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&addressdetails=1&format=json`;
+export const BASE_WEATHER_URL = (lat, lon) =>
+  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily,hourly,minutely&units=metric&appid=${WEATHER_API_KEY}`;
