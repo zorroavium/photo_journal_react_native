265f39601abae1cc4fc01f9b3cca5e264f103fbc
Rahul Prasad <rahulp@polyglotconsultants.com>
2022-01-04 17:05:30 +0530

    Update rootnavigator for back icon customization using remixicon and code refactor

--- picturejournalapp/src/app/navigation/RootNavigator.js
+++ picturejournalapp/src/app/navigation/RootNavigator.js
@@ -1,4 +1,4 @@
-import { TouchableOpacity, View, StyleSheet} from 'react-native';
+import { TouchableOpacity, View, StyleSheet, LogBox } from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
@@ -6,20 +6,25 @@ import Icon from 'react-native-remix-icon';
 import 'react-native-gesture-handler';
 import React from 'react';
 
-import Splash from '../global/splash';
 import HomeScreen from '../screens/Home/Home';
 import DayView from '../screens/DayView/DayView';
-import Summary from '../screens/Summary/Summary';
-import OpenCamera from '../components/openCamera';
-import screens from '../constants/screenConstants';
-import CustomTitle from '../components/customTitle';
+import InfoScreen from '../screens/Summary/InfoScreen';
 import PhotoView from '../screens/PhotoView/PhotoView';
+import CameraTab from '../screens/CameraTab/cameraTab';
+import CameraView from '../screens/CameraView/cameraView';
+
+import Screens from '../constants/screenConstants';
+import CustomTitle from '../components/customTitle';
 import ScreenOptionsIcon from '../components/screenOptionsIcon';
 import CustomTabBarButton from '../components/customTabBarButton';
 
 const Stack = createNativeStackNavigator();
-const StackPhotoView = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const icons = {
+  addLine: {label : 'add-line', color: '#00e3ba'},
+  backArraow: {label : 'arrow-left-s-line'},
+}
 
 const options = {
   tabBarInactiveTintColor: '#FFFFFF',
@@ -30,65 +35,98 @@ const options = {
   tabBarShowLabel: false,
 };
 
-const photoStack = () => {
-  return (
-    <StackPhotoView.Navigator>
-      <Stack.Screen
-        options={{headerShown: false}}
-        name="PhotoViewNested"
-        component={PhotoView}
-      />
-    </StackPhotoView.Navigator>
-  );
-};
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
-const ScreenStack = ({route}) => {
+const ScreenStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={screens.Splash}>
+      <Stack.Navigator
+        initialRouteName={Screens.Splash}
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerBackVisible: false,
+          headerTitle: () => (
+            <CustomTitle
+              hasNavigationArrow={true}
+              text1="pic"
+              text2="a"
+              text3="day"
+            />
+          ),
+          headerStyle: {},
+        }}>
         <Stack.Screen
-          name={screens.Splash}
-          options={{headerShown: false}}
-          component={Splash}
-        />
-        <Stack.Screen
-          name={screens.Home}
-          options={{headerShown: false}}
+          name={Screens.Home}
           component={Home}
+          options={{headerShown: false}}
         />
-        <Stack.Screen name="photoStack" component={photoStack}/>
+        <Stack.Screen
+          name={Screens.PhotoView}
+          component={PhotoView}
+          options={({navigation}) => ({
+            headerLeft: () => (
+              <Icon
+                containerStyle={styles.backArrowContainerStyle}
+                name={icons.backArraow.label}
+                size={24}
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              />
+            ),
+          })}
+        />
+        <Stack.Screen
+          name={Screens.DayView}
+          component={DayView}
+          options={({navigation}) => ({
+            headerLeft: () => (
+              <Icon
+                containerStyle={styles.backArrowContainerStyle}
+                name={icons.backArraow.label}
+                size={24}
+                onPress={() => {
+                  navigation.navigate(Screens.Home);
+                }}
+              />
+            ),
+          })}
+        />
+        <Stack.Screen name={Screens.CameraView} component={CameraView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <Tab.Navigator
-      initialRouteName={screens.HomeScreen}
+      initialRouteName={Screens.HomeScreen}
       screenOptions={({route}) => ({
         ...options,
-        tabBarIcon: ({color, focused}) => (
+        tabBarIcon: ({focused}) => (
           <ScreenOptionsIcon focused={focused} routeName={route.name} />
         ),
         headerTitleAlign: 'center',
         headerStyle: {height: 80},
       })}>
       <Tab.Screen
-        name={screens.HomeScreen}
+        name={Screens.HomeScreen}
         component={HomeScreen}
         options={{
           headerTitle: () => <CustomTitle text1="pic" text2="a" text3="day" />,
         }}
       />
       <Tab.Screen
-        name="Day View"
-        component={DayView}
+        name={Screens.CameraTab}
+        component={CameraTab}
         options={{
-          tabBarIcon: ({focused, color}) => (
+          tabBarIcon: () => (
             <View>
-              <TouchableOpacity onPress={OpenCamera} style={styles.roundButton}>
-                <Icon name="add-line" size={24} color="#00e3ba" />
+              <TouchableOpacity
+                onPress={() => navigation.navigate(Screens.CameraView)}
+                style={styles.roundButton}>
+                <Icon name={icons.addLine.label} size={24} color={icons.addLine.color} />
               </TouchableOpacity>
             </View>
           ),
@@ -96,8 +134,8 @@ const Home = () => {
         }}
       />
       <Tab.Screen
-        name={screens.Summary}
-        component={Summary}
+        name={Screens.Info}
+        component={InfoScreen}
         options={{
           headerTitle: () => <CustomTitle text1="pic" text2="a" text3="day" />,
         }}
@@ -113,6 +151,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  backArrowContainerStyle: {
+    position: 'relative',
+    marginLeft: 15,
+    marginRight: 0,
+    left: 30,
   },
   roundButton: {
     width: 70,
