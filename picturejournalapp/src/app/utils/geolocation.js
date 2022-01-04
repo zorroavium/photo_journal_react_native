0aaf411b05435c67aa3d42e920951105158e7a89
Rahul Prasad <rahulp@polyglotconsultants.com>
2022-01-04 17:02:58 +0530

    Geolocation code refactor

--- picturejournalapp/src/app/utils/geolocation.js
+++ picturejournalapp/src/app/utils/geolocation.js
@@ -4,7 +4,7 @@ import {Platform, PermissionsAndroid} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 
 import {
-  saveLocation,
+  saveCoordinates,
   getLocationDetails,
   getTemperature,
 } from '../screens/store/actions';
@@ -19,11 +19,11 @@ const GeoLocation = props => {
   useEffect(() => {
     if (watchID) {
       Geolocation.clearWatch(watchID);
-    }
+    } 
     getLocationFromUserDevice();
   }, []);
 
-  const saveUserLocation = location => dispatch(saveLocation(location));
+  const saveUserLocationCoordinates = location => dispatch(saveCoordinates(location));
   const getLocationDetailsUsingCoordinates = location => dispatch(getLocationDetails(location));
   const getTemperatureInfo = location => dispatch(getTemperature(location));
 
@@ -58,9 +58,9 @@ const GeoLocation = props => {
       position => {
         console.log('location: the position is ', position);
         setInitialPosition({position});
-        saveUserLocation(position.coords);
-        getLocationDetailsUsingCoordinates(position.coords);
+        saveUserLocationCoordinates(position.coords);
         getTemperatureInfo(position.coords);
+        getLocationDetailsUsingCoordinates(position.coords);
       },
       error => console.log('location: the error is ', error, error.message),
       {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
