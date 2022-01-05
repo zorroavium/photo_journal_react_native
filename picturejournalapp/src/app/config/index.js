d723fdec10920e2455af11e142caa11a4f28a89f
Rahul Prasad <rahulp@polyglotconsultants.com>
2022-01-05 20:45:10 +0530

    update global config

--- picturejournalapp/src/app/config/index.js
+++ picturejournalapp/src/app/config/index.js
@@ -1,4 +1,15 @@
 const WEATHER_API_KEY = '82f86f8855eeda9332684d7cc04d50d2';
+
+export const IMAGE_ENTRY_KEY =
+  new Date()
+    .toLocaleDateString()
+    .replaceAll('/', '-')
+    .replaceAll(',', '-')
+    .replaceAll(':', '-')
+    .replaceAll(' ', '-') + '.jpg';
+
+ export const IMAGE_URI = (image) => `file://${image + '?random=' + new Date()}`;
+
 export const BASE_URL_LOCATION = (lat, lon) =>
   `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&addressdetails=1&format=json`;
 export const BASE_WEATHER_URL = (lat, lon) =>
